031b164aaa5a7d1a6632a2a9236985aa8dc15048
coders-sohan <coders.sohan.0216@gmail.com>
2021-10-29 22:08:56 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,101 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>Header here</h1>
-        </div>
-    );
+	return (
+		<>
+			<div>
+				<nav className="navbar navbar-expand-lg navbar-light bg-light">
+					<div className="container">
+						<NavLink className="navbar-brand" to="/">
+							{/* <img src="{logo}" alt="logo" width="160px" /> */}
+                            Logo
+						</NavLink>
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarSupportedContent"
+							aria-controls="navbarSupportedContent"
+							aria-expanded="false"
+							aria-label="Toggle navigation"
+						>
+							<span className="navbar-toggler-icon"></span>
+						</button>
+						<div
+							className="collapse navbar-collapse"
+							id="navbarSupportedContent"
+						>
+							<ul className="navbar-nav ms-auto  mb-2 mb-lg-0">
+								<li className="nav-item me-3">
+									<NavLink
+										className="navbar-item nav-link"
+										aria-current="page"
+										to="/home"
+									>
+										Home
+									</NavLink>
+								</li>
+								<li className="nav-item me-3">
+									<NavLink className="navbar-item nav-link" to="/about">
+										About
+									</NavLink>
+								</li>
+								<li className="nav-item me-3">
+									<NavLink className="navbar-item nav-link" to="/services">
+										Services
+									</NavLink>
+								</li>
+								<li className="nav-item me-3">
+									<NavLink className="navbar-item nav-link" to="/contact">
+										Contact
+									</NavLink>
+								</li>
+
+								{/* {!user?.email && (
+									<>
+										<li className="nav-item me-3">
+											<NavLink className="navbar-item nav-link" to="/login">
+												Login
+											</NavLink>
+										</li>
+										<li className="nav-item me-3">
+											<NavLink className="navbar-item nav-link" to="/register">
+												Register
+											</NavLink>
+										</li>
+									</>
+								)}
+
+								{user?.email && (
+									<>
+										<li className="nav-item me-3">
+											{
+												<div className="navbar-item nav-link">
+													<img
+														className="rounded-circle"
+														height="28px"
+														width="28px"
+														src={user.photoURL}
+														alt="img"
+													/>
+													<span className="ms-2">{user.displayName}</span>
+												</div>
+											}
+										</li>
+										<button onClick={logOut} className="btn btn-login-logout">
+											Log out
+										</button>
+									</>
+								)} */}
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</>
+	);
 };
 
 export default Header;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>This is home page</h1>
-        </div>
-    );
+	return (
+		<>
+			<div className="container">
+				<h1>This is home page</h1>
+			</div>
+		</>
+	);
 };
 
 export default Home;
